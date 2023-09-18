9c86327c8420986c1695b13639d20e6760708241
Andreas Christiansen <andycchhrr@gmail.com>
2023-09-18 13:38:44 +0200

    formatting/comments

--- a1.php
+++ a1.php
@@ -8,64 +8,69 @@
 </head>
 
 <body>
+    <div style="margin-left: 2vw">
+        <h2>1.</h2>
 
-    <?php
-    // 1.
-    $courseGrades = array(
-        array("Webcoding:", 88),
-        array("UCD:", 66),
-        array("Graphic Design Prinsiples:", 95),
-        array("Exphil:", 13)
-    );
+        <?php
 
+        // 1.
+        $courseGrades = array(
+            // Define array of course grades.
+            array("Webcoding:", 88),
+            array("User Centred Design:", 66),
+            array("Graphic Design Prinsiples:", 80),
+            array("Exphil:", 13)
+        );
 
-    $checkGrade = function ($gradeArray) {
+        // Define a function to check and display grades.
+        function checkGrade($gradeArray)
+        {
+            foreach ($gradeArray as $course) {
+                $name = $course[0];
+                $grade = $course[1];
 
-        foreach ($gradeArray as $course) {
-            $name = $course[0];
-            $grade = $course[1];
-            if ($grade >= 85 && $grade <= 100) {
-                echo "$name A<br>";
-            } elseif ($grade >= 75 && $grade <= 84) {
-                echo "$name B<br>";
-
-            } elseif ($grade >= 65 && $grade <= 74) {
-                echo "$name C<br>";
-
-            } elseif ($grade >= 50 && $grade <= 64) {
-                echo "$name D<br>";
-
-            } elseif ($grade < 50 && $grade >= 0) {
-                echo "$name F<br>";
-
-            } else {
-                echo "Error - $name does not a valid score<br>";
+                // Check the grade and assign a corresponding letter grade.
+                if ($grade >= 85 && $grade <= 100) {
+                    echo "$name A";
+                } elseif ($grade >= 75 && $grade <= 84) {
+                    echo "$name B";
+                } elseif ($grade >= 65 && $grade <= 74) {
+                    echo "$name C";
+                } elseif ($grade >= 50 && $grade <= 64) {
+                    echo "$name D";
+                } elseif ($grade < 50 && $grade >= 0) {
+                    echo "$name F";
+                } else {
+                    echo "Error - $name does not a valid score"; // Display an error message for invalid grades.
+                }
+                echo "<br>";
             }
         }
-    };
-
-    $checkGrade($courseGrades);
 
 
-    echo "<br><br><br><br><br><br>";
+        // Call function.   
+        checkGrade($courseGrades);
 
-    // 2.  
-    
-    $printStars = function ($nr) {
 
-        for ($i = $nr; $i > 0; $i--) {
-            for ($j = $i; $j > 0; $j--) {
-                echo "*";
+        // 2.  
+        echo "<h2>2.</h2>";
+
+        // Define function.
+        function printStar($nr)
+        {
+            for ($i = $nr; $i > 0; $i--) { // Loop controls number of lines printed.  
+                for ($j = $i; $j > 0; $j--) { // Loop controls number of asterisks printed in each line.
+                    echo "*";
+                }
+                echo "<br>";
             }
-            echo "<br>";
-        };
+        }
 
-    };
+        // Call function. Para = max number of asterisks printed.
+        printStar(25);
 
-    $printStars(20);
-
-    ?>
-
+        ?>
+    </div>
 </body>
 
 </html>
